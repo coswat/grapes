a6d8e2083cb90a0b89e14d091a3b59047375fb7e
coderswat <abhishek.b4696@gmail.com>
2023-03-01 19:19:14 +0530

    updated cs fix

--- tests/GrapeTest.php
+++ tests/GrapeTest.php
@@ -4,29 +4,29 @@ use PHPUnit\Framework\TestCase;
 use Coswat\Grapes\Grape;
 class GrapeTest extends TestCase
 {
-  public function test_toYear_function(): void
-  {
-     $year =  Grape::time(time())->toYear();
-     $expected = 2023;
-     $this->assertEquals($year,$expected);
-  }
-  public function test_toMonth_function(): void
-  {
-     $month = Grape::time(time())->toMonth();
-     $expected = 'March';
-     $this->assertEquals($month,$expected);
-  }
-  public function test_toDay_function(): void
-  {
-    $day = Grape::time(time())->toDay();
-    $expected = 'Wednesday';
-    $this->assertEquals($day,$expected);
-  }
-  public function test_getTimezone_function(): void 
-  {
-    $ip = '8.8.8.8';
-    $timeZone = Grape::getTimezone($ip);
-    $expected = 'America/New_York';
-    $this->assertEquals($timeZone,$expected);
-  }
+    public function test_toYear_function(): void
+    {
+        $year = Grape::time(time())->toYear();
+        $expected = 2023;
+        $this->assertEquals($year, $expected);
+    }
+    public function test_toMonth_function(): void
+    {
+        $month = Grape::time(time())->toMonth();
+        $expected = "March";
+        $this->assertEquals($month, $expected);
+    }
+    public function test_toDay_function(): void
+    {
+        $day = Grape::time(time())->toDay();
+        $expected = "Wednesday";
+        $this->assertEquals($day, $expected);
+    }
+    public function test_getTimezone_function(): void
+    {
+        $ip = "8.8.8.8";
+        $timeZone = Grape::getTimezone($ip);
+        $expected = "America/New_York";
+        $this->assertEquals($timeZone, $expected);
+    }
 }
